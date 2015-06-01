a12521a967a330a398157dad12be1f49ab6867b2
Stefan Penner <stefan.penner@gmail.com>
2015-06-01 13:53:24 -0700

    [fixes #25] upgrade ember-cli

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return [
+    return Ember.A([
       { id: 1, subject: 'Recruiter spam',            from: 'some@example.com', body: 'long message' },
       { id: 2, subject: 'other Recruiter spam',      from: 'some@example.com', body: 'long message' },
       { id: 3, subject: 'more Recruiter spam',       from: 'some@example.com', body: 'long message' },
       { id: 4, subject: 'additional Recruiter spam', from: 'some@example.com', body: 'long message' },
-    ];
+    ]);
   }
 });
