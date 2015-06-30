bd920835a686a6ead3feabebe9c7a0f66819e8b9
Stefan Penner <stefan.penner@gmail.com>
2015-06-30 16:08:17 -0700

    fix example

--- tests/dummy/app/components/edit-email.js
+++ tests/dummy/app/components/edit-email.js
@@ -3,18 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'form',
   editEmailService: Ember.inject.service('edit-email'),
-  state: Ember.computed('email', function() {
+  data: Ember.computed('email', function() {
     return this.get('editEmailService').stateFor(this.get('email'));
   }).readOnly(),
 
   actions: {
     save: function() {
-      this.get('state').applyChanges();
+      this.get('data').applyChanges();
       this.sendAction('on-save', this.get('email'));
     },
 
     cancel: function() {
-      this.get('state').discardChanges();
+      this.get('data').discardChanges();
       this.sendAction('on-cancel', this.get('email'));
     }
   }
