4f0bc68e134da70f30404ee99798f825280a5005
Travis Hoover <thoov7@gmail.com>
2015-09-03 16:25:26 -0700

    Adding builder method for initialState

--- addon/state-for.js
+++ addon/state-for.js
@@ -2,41 +2,79 @@ import Ember from 'ember';
 
 var {
   computed,
-  assert
+  assert,
+  typeOf
 } = Ember;
 
 var states = {};
 
-function createState(container, stateName) {
-  const stateContainerName = `state:${stateName}`;
-  const StateFactory = container.lookupFactory(stateContainerName);
+/*
+* Create the states object for a given stateName.
+*
+* @param container {object} container to lookup the state factory on
+* @param stateName {string} state name which is also the name of the factory
+*/
+function createStates(container, stateName) {
+  let stateContainerName = `state:${stateName}`;
+  let StateFactory       = container.lookupFactory(stateContainerName);
 
-  if (StateFactory === undefined) {
-    throw new TypeError('Unknown StateFactory: `' + stateContainerName + '`');
+  if (!StateFactory) {
+    throw new TypeError(`Unknown StateFactory: \`${stateContainerName}\``);
   }
 
   states[stateName] = new Ember.MapWithDefault({
-    defaultValue: key => setupState(StateFactory, key)
+    defaultValue: () => buildDefaultState.call(this, StateFactory)
   });
 
-  return states;
+  return states[stateName];
 }
 
-function setupState(Factory, model) {
-  return Factory.create({
-    content: model
-  });
+/*
+* When creating the state instance we use `initialState` method
+* on the state class to build its initial state. If it not
+* specified then {} is used as the default state.
+*
+* @param Factory {StateFactoryClass} state factory from the container
+* @return {stateInstance} state instance object from the factory
+*/
+function buildDefaultState(Factory) {
+  let defaultState = {};
+
+  if (typeOf(Factory.initialState) === 'function') {
+    defaultState = Factory.initialState.call(this);
+  }
+
+  return Factory.create(defaultState);
 }
 
-export default function(stateName, options) {
-  assert('key property must be passed in via the second parameter', options.key);
+/*
+* Returns a computed property that returns state based off of a dynamic key.
+*
+* @param stateName {string} name of the state factory which is located in /states/<stateName>.js
+* @param options {object}
+* @param {string} options.key - required - the dynamic state key
+* @param {object} options.container - optional - container reference
+* @return {computed property}
+*/
+export default function stateFor(stateName, options) {
+  var { key, container } = options;
 
-  return computed(options.key, function() {
+  assert(`
+    Missing \`key\` property within the second argument. You passed: ${JSON.stringify(options)}
+    `, key);
+
+  return computed(key, function() {
+    assert(`
+      Could not find the container on \`this\` or passed in via:
+      stateFor('${stateName}', { key: ${key}, container: <pass container here> })
+      `, this.container || container);
+
     if (states[stateName]) {
-      return states[stateName].get(this.get(options.key));
+      return states[stateName].get(this.get(key));
     }
 
-    createState(this.container, stateName);
-    return states[stateName].get(this.get(options.key));
+    return createStates
+              .apply(this, [container || this.container, stateName])
+              .get(this.get(key));
   });
 }
