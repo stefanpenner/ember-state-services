a13d8422783998756e5e98912727c2de84330a27
Travis Hoover <thoov7@gmail.com>
2015-09-17 16:24:14 -0700

    Address PR comments

--- addon/state-for.js
+++ addon/state-for.js
@@ -3,48 +3,47 @@ import WeakMap from 'ember-weakmap/weak-map';
 
 var {
   computed,
-  assert,
-  typeOf
+  assert
 } = Ember;
 
 /*
  * Each key in this POJO is a name of a state with the value
  * for that key being a WeakMap.
  */
-var stateHashMap = {};
+var weakMaps = {};
 
 /*
 * Returns a computed property if called like: stateFor('state-name', 'property-name') or
 * returns the WeakMap if called like stateFor('state-name'). The most common case will be 
 * the former but the latter allows advanced options like:
 *
-* stateFor('state-name').delete(this.get('property-name');
-* stateFor('state-name').has(this.get('some-other-prop');
+* stateFor('state-name').delete(this.get('property-name'));
+* stateFor('state-name').has(this.get('some-other-prop'));
 * stateFor('state-name').set(this.get('property-name'), {}); // Overrides the state
 * stateFor('state-name').get(this.get('property-name'));
 */
 export default function stateFor(stateName, propertyName) {
   if (!propertyName) {
-    return stateHashMap[stateName];
+    return weakMaps[stateName];
   }
 
-  assert('The second argument must be a string', typeOf(propertyName) === 'string');
+  assert('The second argument must be a string', typeof(propertyName) === 'string');
 
   return computed(propertyName, function() {
     let propertyValue = this.get(propertyName);
 
-    if (!stateHashMap[stateName]) {
-      stateHashMap[stateName] = new WeakMap();
+    if (!weakMaps[stateName]) {
+      weakMaps[stateName] = new WeakMap();
     }
 
-    let stateWeakMap = stateHashMap[stateName];
+    let state = weakMaps[stateName];
 
-    if (!stateWeakMap.has(propertyValue)) {
+    if (!state.has(propertyValue)) {
       let newState = createStateFor(this, stateName, this.container);
-      stateWeakMap.set(propertyValue, newState);
+      state.set(propertyValue, newState);
     }
 
-    return stateWeakMap.get(propertyValue);
+    return state.get(propertyValue);
   });
 }
 
@@ -61,8 +60,11 @@ function createStateFor(context, stateName, container) {
     throw new TypeError(`Unknown StateFactory: ${containerName}`);
   }
 
-  if (typeOf(StateFactory.initialState) === 'function') {
+  if (typeof(StateFactory.initialState) === 'function') {
     defaultState = StateFactory.initialState.call(context);
+  }
+  else if (StateFactory.initialState) {
+    throw new TypeError('initialState property must be a function');
   }
 
   if (StateFactory.create) {
